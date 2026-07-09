27a45cd043f7d4b2b8ad13448dcab1b93c7fe4f9
codesimply29-source <codesimply29@gmail.com>
2026-07-09 18:29:06 +0300

    minimal test

--- server/app.js
+++ server/app.js
@@ -1,43 +1,17 @@
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
-const path = require("path");
-
-const connectDB = require("./database");
-
-const authRoutes = require("./routes/auth");
-const courseRoutes = require("./routes/courseRoutes");
-const PaymentRoutes = require("./routes/PaymentRoutes");
 
 const app = express();
 
-// الاتصال بقاعدة البيانات
-connectDB();
+app.get("/", (req, res) => {
+    res.send("Server Works");
+});
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// عرض الصور المرفوعة
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// ملفات الموقع
-
-app.use(express.static(path.join(__dirname, "..")));
 app.get("/test", (req, res) => {
     res.send("OK");
 });
-// API Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/courses", courseRoutes);
-app.use("/api/payments", PaymentRoutes);
 
-// تشغيل السيرفر
-const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-    res.send("Home Works");
-});
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-    console.log(`✅ Server Running On Port ${PORT}`);
+    console.log(`Running on ${PORT}`);
 });
